654162154daa5938abdba7b4b36370c47347d0d7
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-15 15:13:31 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura (número impar mayor o igual a 5): "));
+
+        int ancho = 6;
+
+        if (altura < 5 || altura % 2 != 0) {
+            System.out.println("La altura introducida no es correcta.");
+        }
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < ancho; j++) {
+                if (i == 0 || i == altura - 1 || j == 0 || j == ancho - 1 || i == altura/2)
+                    System.out.print("M");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
